24f1086ded566be4d012f1768f5cb9b499d2aa7e
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-03-09 02:00:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     API_HASH = "346becfc02417e50a2837a8d545b015e"
     TOKEN = os.environ.get("TOKEN", 6821626224:AAFp83mH4nNSo3DVHqehV0B1pf-HuvABEaA)
     MONGO_URL = "mongodb+srv://mdmatinashraf43:gymoW5DpuqoKf9xg@cluster0.s2falsa.mongodb.net/?retryWrites=true&w=majority"
-    START_PIC = "https://telegra.ph/file/a8ba8edd60489a54f2f84.jpg"
+    START_PIC = "https://telegra.ph/file/1f699ca115629afb0e092.jpg"
     SUDOERS = filters.user(["5970997865"])
